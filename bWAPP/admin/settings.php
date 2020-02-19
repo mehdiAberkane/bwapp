3662e2fde3a915fd029655e64f484f9fb92f0938
Mehdi <mehdi@mehdi-aberkane.fr>
2020-02-19 02:52:55 +0100

    update config

--- bWAPP/admin/settings.php
+++ bWAPP/admin/settings.php
@@ -17,7 +17,7 @@ bWAPP is licensed under a Creative Commons Attribution-NonCommercial-NoDerivativ
 */
 
 // Database connection settings
-$db_server = "localhost";
+$db_server = "127.0.0.1";
 $db_username = "chevre";
 $db_password = "chevre";
 $db_name = "bWAPP";
